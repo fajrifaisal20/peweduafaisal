285ed19a038cb0193be832ce9c49dd4f9d6313ac
faisalfajri20 <muh.faisal.fajri@student.untan.ac.id>
2018-05-09 16:22:12 +0700

    update

--- content.php
+++ content.php
@@ -10,7 +10,7 @@ include_once 'navigation.php';
           <div class="">
             <div class="page-title">
               <div class="title_left">
-                <h3>Plain Page</h3>
+                <h3>Plain Page3</h3>
               </div>
 
               <div class="title_right">
